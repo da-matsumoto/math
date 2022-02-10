5b15047e05ff3be8aa097a477ced800dc29949fb
DESKTOP-3DPFR2G\daisu <daisuke.matsumoto@mediwill.co.jp>
2022-02-10 23:46:16 +0900

    枠線と塗りつぶし

--- example.js
+++ example.js
@@ -1,8 +1,25 @@
 function setup() {
+	// 線
 	ctx.beginPath();
 	ctx.moveTo(200, 100);
 	ctx.lineTo(800, 900);
 	ctx.stroke();
+
+	// 枠線
+	ctx.beginPath();
+	ctx.moveTo(0, 0);
+	ctx.lineTo(screenWidth, screenHeight);
+	ctx.lineTo(screenWidth/3, screenHeight);
+	ctx.lineTo(0, 0);
+	ctx.stroke();
+
+	// 塗りつぶし
+	ctx.beginPath();
+	ctx.moveTo(screenWidth*0.5, screenHeight*0.1);
+	ctx.lineTo(screenWidth, 0);
+	ctx.lineTo(screenWidth*0.8, screenHeight*0.6);
+	ctx.lineTo(screenWidth*0.4, screenHeight*0.2);
+	ctx.fill();
 }
 
 function loop() {
