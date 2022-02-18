690a9db3a77342190f721010fe89679835c5ca4c
DESKTOP-3DPFR2G\daisu <daisuke.matsumoto@mediwill.co.jp>
2022-02-18 23:47:03 +0900

    円弧を描く

---
+++ example2.js
@@ -0,0 +1,22 @@
+function setup() {
+	// 円弧
+  ctx.beginPath();
+  // 中心のX座標,中心のY座標,半径,開始角度,終了角度,半時計回りに描くのか？
+  ctx.arc(500, 200, 100, 30/180*Math.PI, 120/180*Math.PI, true);
+  ctx.stroke();
+}
+
+function loop() {
+}
+
+function touchStart() {
+	
+}
+
+function touchMove() {
+	
+}
+
+function touchEnd() {
+	
+}
