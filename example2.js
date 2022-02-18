6229066444448472eff0bf5c7d8589bdd51e1195
DESKTOP-3DPFR2G\daisu <daisuke.matsumoto@mediwill.co.jp>
2022-02-18 23:56:03 +0900

    楕円を描く

--- example2.js
+++ example2.js
@@ -4,6 +4,15 @@ function setup() {
   // 中心のX座標,中心のY座標,半径,開始角度,終了角度,半時計回りに描くのか？
   ctx.arc(500, 200, 100, 30/180*Math.PI, 120/180*Math.PI, true);
   ctx.stroke();
+
+  // これ以降に描く線は14になる
+  ctx.lineWidth = 14;
+
+  // 楕円
+  ctx.beginPath();
+  // 中心のX座標,中心のY座標,横半径,縦半径,傾き,開始角度,終了角度,半時計回りに描くのか？
+  ctx.ellipse(500, 600, 200, 100, 45/180*Math.PI, 30/180*Math.PI, 120/180*Math.PI, true);
+  ctx.stroke();
 }
 
 function loop() {
