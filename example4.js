5403e1dbd49ac734dea974ffee8780c11002d458
DESKTOP-3DPFR2G\daisu <daisuke.matsumoto@mediwill.co.jp>
2022-02-20 23:18:08 +0900

    文字を試す

---
+++ example4.js
@@ -0,0 +1,34 @@
+function setup() {
+  ctx.fillText("Designing Math", screenWidth/2, 200);
+
+  ctx.strokeStyle = "black";
+  ctx.font = "60px Serif";
+  ctx.textAlign = "right";
+  ctx.strokeText("Designing Math", screenWidth/2, 400);
+  
+  ctx.fillStyle = "grey";
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 4;
+  ctx.font = "80px Helvitica";
+  ctx.textAlign = "center";
+  ctx.strokeText("Designing Math", screenWidth/2, 600);
+  ctx.fillText("Designing Math", screenWidth/2, 600);
+
+  ctx.fillText("Designing Math", screenWidth/2, 800);
+  ctx.strokeText("Designing Math", screenWidth/2, 800);
+}
+
+function loop() {
+}
+
+function touchStart() {
+	
+}
+
+function touchMove() {
+	
+}
+
+function touchEnd() {
+	
+}
